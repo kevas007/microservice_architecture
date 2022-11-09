9a9ec1d830f3f4fdb278bd6ce1dfba474cedf6a9
kevas007 <kevassiobo@gmail.com>
2022-11-09 15:01:33 +0100

    [DEV]: update sever

--- src/auth/server.py
+++ src/auth/server.py
@@ -21,8 +21,32 @@ def login():
 	if not  auth :
 		return "missing credentails" , 401
 	# check db for username and password
+	cur  = mysql.connection.cursor()
+	res  = cur.execute(
+		"SELECT email, password FROM user WHERE email=%S", (auth.username,)
+	)
+	if res > 0:
+		user_row  = cur.fetchone()
+		email = user_row[0]
+		password = user_row[1]
+		if auth.username != email or  auth.password != passord:
+			return "invalid credentials", 401
+		else:
+			return  createJWT(auth.username, os.environ.get("JWT_SECRET"), True)		
+	else:
+    		return "invalide credentials", 401
 
+def createJWT(username, secret, authz):
+	return jwt.encode(
+		{
+			"username": username,
+			"exp" : datetime.datetime.now(tz = datetime.timezone.utc) + datetime.timedelta(day=1),
+			"iat": datetime.datetime.utcnow(),
+			"admin": authz,
+		},
+		secret,
+		algorithm= "HS256",
+)
 
-
-
-
+if __name__ == "__main__":
+	server.run(host="0.0.0.0", port=5000)
